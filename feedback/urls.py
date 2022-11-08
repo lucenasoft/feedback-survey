3711649f4a21cb37737a94f217095d46f773ef70
lucenasoft <getugbr@gmail.com>
2022-11-08 15:54:38 -0300

    finished project

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'feedback'
+
 urlpatterns = [
-    path('', views.feedback, name='feedback')
+    path('', views.feedback, name='feedback'),
+    path('add', views.feedback_create, name='feedback_create'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterFeedback
 
@@ -10,3 +11,20 @@ def feedback(request):
     return render(request, 'pages/home.html', {
         'form': form,
     })
+
+
+def feedback_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterFeedback(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Enviado com sucesso')
+        del (request.session['register_form_data'])
+        return redirect(reverse('feedback:feedback'))
+
+    return redirect('feedback:feedback')
